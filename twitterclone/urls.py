7d5abb49d793c3ffb79cfa298ffea41185ec8839
Jackson Nathaniel Detke <jndetke2150@gmail.com>
2020-09-11 10:57:16 -0400

    Turned four function-based views into class-based views.

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -22,13 +22,13 @@ from authentication import views as auth_view
 from notification import views as note_view
 
 urlpatterns = [
-    path('', user_view.homepage_view, name="homepage"),
-    path('profilepage/<int:user_id>/', user_view.profilepage_view, name="profilepage"),
-    path('tweetpage/<int:tweet_id>/', user_view.tweetpage_view, name="tweetpage"),
+    path('', user_view.HomepageView.as_view(), name="homepage"),
+    path('profilepage/<int:user_id>/', user_view.ProfilePageView.as_view(), name="profilepage"),
+    path('tweetpage/<int:tweet_id>/', user_view.TweetPageView.as_view(), name="tweetpage"),
     path('notifications/', note_view.notification_view, name="notifications"),
     path('follow/<int:user_id>/', user_view.follow_view, name="follow"),
     path('unfollow/<int:user_id>/', user_view.unfollow_view, name="unfollow"),
-    path('tweet/', tweet_view.create_tweet_view, name="tweet"),
+    path('tweet/', tweet_view.CreateTweetFormView.as_view(), name="tweet"),
     path('login/', auth_view.login_view, name="login"),
     path('signup/', auth_view.signup_view, name="signup"),
     path('logout/', auth_view.logout_view, name="logout"),
